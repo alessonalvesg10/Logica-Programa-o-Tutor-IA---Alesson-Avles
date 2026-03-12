f3df2084b97c7a48555d36719f436cfe95ea7bc2
Alesson Alves <alesson.alvesg10@gmail.com>
2026-03-12 16:00:27 -0300

    Atividade 1

--- Aula 03/Atividades.java
+++ Aula 03/Atividades.java
@@ -1,1 +1,13 @@
-teste
+import java.util.Scanner;
+public class Atividades {
+  public static  void main(String[] args) {
+  Scanner vox = new Scanner (System.in);
+  System.out.print("Qual seu nome? ");
+  String Nome = vox.nextLine();
+  System.out.print("Qual sua idade? ");
+  int idade = vox.nextShort();
+  System.out.println("Olá " + Nome + " você tem " + idade + " anos ");
+  vox.close();  
+
+  }
+}
