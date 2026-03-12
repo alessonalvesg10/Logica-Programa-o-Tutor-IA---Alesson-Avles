c2c229343bd250a34602767751126acf2a6de85c
Alesson Alves <alesson.alvesg10@gmail.com>
2026-03-12 14:02:12 -0300

    Atualização nome aula 1

---
+++ Aula 02/Atividade.java
@@ -0,0 +1,7 @@
+public class Atividade {
+    public static void main(String[] args) {
+        System.out.println("Salve meu mano!");
+        int idade = 22;
+        System.out.println(idade);
+    }
+}
